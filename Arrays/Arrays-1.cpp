b6b98619a7fabc83d5b694cd40b2b3f22a239faf
sayyadArslan <arslan5sherazi@gmail.com>
2025-07-22 14:07:36 +0500

    Arrays Practice

---
+++ Arrays/Arrays-1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int arr[5]= {10,20,30,40,50};
+	for(int i=0;i<5;i++)
+	{
+		cout<<"i="<<arr[i]<<endl;
+	}
+	for(int i=4;i>=0;i--)
+	{
+		cout<<"i="<<arr[i]<<endl;
+	}
+	cout<<"Arslan";
+	return 0;
+}
